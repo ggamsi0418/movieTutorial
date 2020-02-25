0eb0d526cefecea1ca4fcf09b2cca4ba24fa0f28
ggamsi0418 <58096698+ggamsi0418@users.noreply.github.com>
2020-02-25 21:55:18 +0900

    Update models.py

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30)  # 영화 제목
+    genre = models.CharField(max_length=15)  # 장르
+    year = models.IntegerField()  # 제작 년도
+
+    def __str__(self):
+        return self.title  # admin 페이지에서 'title'이라고 명시된다.
